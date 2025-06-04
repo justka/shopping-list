617fce91e3678fdc8c245c9fb45eede5ef4102c3
Justyna <justka070391@gmail.com>
2025-06-04 15:25:14 +0200

    add eslint, prettier and husky

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import perfectionist from "eslint-plugin-perfectionist";
+import unusedImports from "eslint-plugin-unused-imports";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+
+const selectedConfig = perfectionist.configs["recommended-natural"];
+
+export default [
+  {
+    ...selectedConfig,
+    files: ["**/*.tsx"],
+    languageOptions: {
+      ...selectedConfig.languageOptions,
+      parser: typescriptParser,
+      sourceType: "module",
+    },
+    plugins: {
+      ...selectedConfig.plugins,
+      perfectionist,
+      "unused-imports": unusedImports,
+      "@typescript-eslint": typescriptPlugin,
+    },
+    rules: {
+      ...selectedConfig.rules,
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          vars: "all",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
